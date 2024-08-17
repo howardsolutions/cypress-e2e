342308985db1a5ad973a95454827f4bb94b608d8
thinhphungdev <phungthinh0946519356@gmail.com>
2024-08-17 11:33:47 +0700

    test: test cases for Aliases

--- cypress/integration/03-aliases.spec.js
+++ cypress/integration/03-aliases.spec.js
@@ -1,7 +1,32 @@
+/* eslint-disable no-undef */
 /// <reference types="cypress" />
 
 describe('Aliases', () => {
   beforeEach(() => {
     cy.visit('/jetsetter');
+
+    cy.get('[data-test="filter-items"]').as('filterInput');
+
+    cy.get('[data-test="items"]').as('allItems');
+    cy.get('[data-test="items-unpacked"]').as('unpackedItems');
+    cy.get('[data-test="items-packed"]').as('packedItems');
+  });
+
+  it("should filter items", () => {
+    cy.get('@filterInput').type('iPhone');
+
+    cy.get('@allItems').should('not.contain.text', 'Tshirt')
+  });
+
+  it("should move items from one list to the other", () => {
+    cy.get('@unpackedItems').find('li').first().as('itemInQuestion');
+    cy.get('@itemInQuestion').find('label').as('itemLabel');
+    cy.get('@itemLabel').invoke('text').as('itemName');
+
+    cy.get('@itemLabel').click();
+
+    cy.get('@itemName').then(text => {
+      cy.get('@packedItems').contains(text);
+    })
   });
 });
