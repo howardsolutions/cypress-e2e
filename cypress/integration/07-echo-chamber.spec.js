29a2ae35f4063e73b6c23ffced8a88c3f29fdc75
thinhphungdev <phungthinh0946519356@gmail.com>
2024-08-17 16:59:03 +0700

    test: form validation

--- cypress/integration/07-echo-chamber.spec.js
+++ cypress/integration/07-echo-chamber.spec.js
@@ -6,6 +6,7 @@ describe('Initial Page', () => {
     cy.visit('/echo-chamber');
   });
 
+  
   it('should have the title of the application in the header', () => {
     cy.get('[data-test="application-title"]').should('contain', 'Echo Chamber');
   });
@@ -14,25 +15,63 @@ describe('Initial Page', () => {
     cy.title().should('contain', 'Echo Chamber');
   });
 
+  it('should have a "Sign In" button', () => {
+    cy.get('[data-test="sign-in"]');
+  });
+
+  it('should have a "Sign Up" button', () => {
+    cy.get('[data-test="sign-up"]');
+  });
+
   it('should navigate to "/sign-in" when you click the "Sign In" button', () => {
     cy.get('[data-test="sign-in"]').click();
-    cy.location('pathname').should('contain.text', '/sign-in');
+    cy.location('pathname').should('contain', 'sign-in');
   });
 
   it('should navigate to "/sign-up" when you click the "Sign Up" button', () => {
     cy.get('[data-test="sign-up"]').click();
-    cy.location('pathname').should('contain.text', '/sign-up');
+    cy.location('pathname').should('contain', 'sign-up');
   });
 });
 
 describe('Sign Up', () => {
   beforeEach(() => {
     cy.visit('/echo-chamber/sign-up');
+    cy.get('[data-test="sign-up-email"]').as('email');
+    cy.get('[data-test="sign-up-submit"]').as('submit');
   });
 
-  it('should require an email', () => {});
+  it('should require an email', () => {
+    cy.get('@submit').click();
 
-  it('should require that the email actually be an email address', () => {});
+    cy.get('[data-test="sign-up-email"]:invalid')
+    .invoke('prop', 'validationMessage')
+    .should('contain', 'Please fill out this field')
 
-  it('should require a password when the email is present', () => {});
+
+    // cy.get('[data-test="sign-up-email"]:invalid')
+    // .invoke('prop', 'validationMessage')
+    // .its('valueMissing').should('be.true')
+  });
+
+  it('should require that the email actually be an email address', () => {
+    cy.get('@email').type('invalidemail');
+
+    cy.get('@submit').click();
+
+    cy.get('@email').invoke('prop', 'validationMessage')
+    .should('contain', `Please include an '@' in the email address`);
+    
+    cy.get('[data-test="sign-up-email"]:invalid')
+    .invoke('prop', 'validity')
+    .its('typeMismatch')
+    .should('be.true')
+  });
+
+  it('should require a password when the email is present', () => {
+    cy.get('@email').type('valid@email.com');
+
+    cy.get('[data-test="sign-up-password"]:invalid').invoke('prop', 'validity')
+    .its('valueMissing').should('be.true');
+  });
 });
